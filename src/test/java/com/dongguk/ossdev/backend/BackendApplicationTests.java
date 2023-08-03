4a2911355411fc18d4f3d2aab2721b2d4981918e
jun6292 <c68254@gmail.com>
2023-08-03 15:14:02 +0900

    initial commit

---
+++ src/test/java/com/dongguk/ossdev/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongguk.ossdev.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

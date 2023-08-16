4c1824cc38dfd80e6240f4181f64d283c0dd7f14
WonJun <c68254@gmail.com>
2023-08-16 22:49:30 +0900

    fix: 생활기록부 독서활동상황 CRUD 수정

--- src/main/java/com/dongguk/ossdev/backend/repository/ReadingRepository.java
+++ src/main/java/com/dongguk/ossdev/backend/repository/ReadingRepository.java
@@ -2,7 +2,9 @@ package com.dongguk.ossdev.backend.repository;
 
 import com.dongguk.ossdev.backend.domain.Reading;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReadingRepository extends JpaRepository<Reading, Long> {
 
 }
--- src/main/java/com/dongguk/ossdev/backend/service/ReadingService.java
+++ src/main/java/com/dongguk/ossdev/backend/service/ReadingService.java
@@ -8,6 +8,7 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,28 +18,24 @@ public class ReadingService {
     private final ReadingRepository readingRepository;
 
     @Transactional
-    public ReadingDto create(ReadingRequestDto createRequest) {
-        Reading createReadingEntity = createRequest.toEntity();
-        readingRepository.save(createReadingEntity);
-        ReadingDto createdDto = ReadingDto.builder()
-                .id(createReadingEntity.getId())
-                .grade(createReadingEntity.getGrade())
-                .semester(createReadingEntity.getSemester())
-                .title(createReadingEntity.getTitle())
-                .subject(createReadingEntity.getSubject())
-                .build();
-        return createdDto;
+    public List<ReadingDto> create(List<ReadingRequestDto> createRequest) {
+        List<Reading> createEntityList = createRequest.stream()
+                .map(createReading -> createReading.toEntity())
+                .collect(Collectors.toList());
+
+        createEntityList.stream()
+                .map(saveEntity -> readingRepository.save(saveEntity))
+                        .collect(Collectors.toList());
+
+        List<ReadingDto> createDtos = createEntityList.stream()
+                .map(reading -> ReadingDto.createReadingDto(reading))
+                .collect(Collectors.toList());
+        return createDtos;
     }
 
     public ReadingDto read(Long id) {
         Reading readingEntity = readingRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("독서 활동 상황을 찾을 수 없습니다."));
-        ReadingDto showDto = ReadingDto.builder()
-                .id(readingEntity.getId())
-                .grade(readingEntity.getGrade())
-                .semester(readingEntity.getSemester())
-                .title(readingEntity.getTitle())
-                .subject(readingEntity.getSubject())
-                .build();
+        ReadingDto showDto = ReadingDto.createReadingDto(readingEntity);
         return showDto;
     }
 
@@ -55,26 +52,14 @@ public class ReadingService {
         Reading target = readingRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("독서 활동 상황을 찾을 수 없습니다."));
         target.patch(updateRequest);
         Reading updateReadingEntity = updateRequest.toEntity();
-        ReadingDto updatedDto = ReadingDto.builder()
-                .id(updateReadingEntity.getId())
-                .grade(updateReadingEntity.getGrade())
-                .semester(updateReadingEntity.getSemester())
-                .title(updateReadingEntity.getTitle())
-                .subject(updateReadingEntity.getSubject())
-                .build();
+        ReadingDto updatedDto = ReadingDto.createReadingDto(updateReadingEntity);
         return updatedDto;
     }
 
     @Transactional
     public ReadingDto delete(Long id) {
         Reading target = readingRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("독서 활동 상황을 찾을 수 없습니다."));
-        ReadingDto deletedDto = ReadingDto.builder()
-                .id(target.getId())
-                .grade(target.getGrade())
-                .semester(target.getSemester())
-                .title(target.getTitle())
-                .subject(target.getSubject())
-                .build();
+        ReadingDto deletedDto = ReadingDto.createReadingDto(target);
         readingRepository.delete(target);
         return deletedDto;
     }
